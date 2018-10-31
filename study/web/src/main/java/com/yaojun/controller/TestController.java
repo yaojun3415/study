742993f9cb93eac94c75668b0ca8900a4fe4a87e
yaojun <yaojun3415@yuantutech.com>
2018-10-31 16:45:30 +0800

    +3

--- study/web/src/main/java/com/yaojun/controller/TestController.java
+++ study/web/src/main/java/com/yaojun/controller/TestController.java
@@ -2,6 +2,8 @@ package com.yaojun.controller;
 
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/test")
 public class TestController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @ApiOperation(value="获取测试信息", notes="获取测试信息")
     @ApiImplicitParam(name = "id", value = "测试ID", required = true)
